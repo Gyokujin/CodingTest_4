b4f09c0fcaee60e1b7ba75010567596fca1d2019
Gyokujin <dd43454548@naver.com>
2024-10-07 16:45:57 +0900

    [Bronze V] Title: A+B - 7, Time: 0 ms, Memory: 2056 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int index, numA, numB;
+	cin >> index;
+
+	for (int i = 0; i < index; i++)
+	{
+		cin >> numA >> numB;
+		cout << "Case #" << i + 1 << ": " << numA + numB << endl;
+	}
+}
