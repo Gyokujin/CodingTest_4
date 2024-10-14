3aa25ed873273b3a94b5aef0ab387560a6438563
Gyokujin <dd43454548@naver.com>
2024-10-14 17:42:33 +0900

    [Bronze III] Title: 별 찍기 - 5, Time: 0 ms, Memory: 2056 KB -BaekjoonHub

---
+++ 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int input;
+	cin >> input;
+
+	for (int i = 1; i <= input; i++)
+	{
+		for (int j = 0; j < input - i; j++)
+			cout << ' ';
+
+		for (int j = 0; j < 2 * i - 1; j++)
+			cout << '*';
+
+		cout << endl;
+	}
+}
