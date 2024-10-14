3d8b0d46aa5fc4196969fedb2660d49d973796ca
Gyokujin <dd43454548@naver.com>
2024-10-15 01:29:36 +0900

    [Bronze III] Title: 별 찍기 - 9, Time: 4 ms, Memory: 2056 KB -BaekjoonHub

---
+++ 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int input = 0;
+	cin >> input;
+
+	for (int i = 0; i < input; i++)
+	{
+		for (int j = i; j > 0; j--)
+			cout << ' ';
+
+		for (int j = 2 * (input - i) - 1; j > 0; j--)
+			cout << '*';
+
+		cout << endl;
+	}
+
+	for (int i = input - 1; i > 0; i--)
+	{
+		for (int j = i - 1; j > 0; j--)
+			cout << ' ';
+
+		for (int j = 2 * (input - i); j >= 0; j--)
+			cout << '*';
+
+		cout << endl;
+	}
+}
