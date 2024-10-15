44455815f588a6ea6c046668ecc75b153b1c267d
Gyokujin <dd43454548@naver.com>
2024-10-15 11:47:07 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.01 ms, Memory: 3.62 MB -BaekjoonHub

---
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.cpp
@@ -0,0 +1,23 @@
+#include <string>
+using namespace std;
+
+string solution(string s)
+{
+    string answer = s;
+    int index = 0;
+    
+    for (int i = 0; i < answer.size(); i++)
+    {
+        if (answer[i] == ' ')
+        {
+            index = 0;
+            continue;
+        }
+        else
+            answer[i] = index % 2 == 0 ? toupper(answer[i]) : tolower(answer[i]);
+        
+        index++;
+    }
+
+    return answer;
+}
