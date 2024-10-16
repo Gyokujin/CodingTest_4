1bd303e7bee0aaa7edaf4945852521838b34f5b0
Gyokujin <dd43454548@naver.com>
2024-10-17 00:48:08 +0900

    [Bronze III] Title: 별 찍기 - 12, Time: 4 ms, Memory: 2056 KB -BaekjoonHub

--- 백준/Bronze/2522. 별 찍기 － 12/별 찍기 － 12.cc
+++ 백준/Bronze/2522. 별 찍기 － 12/별 찍기 － 12.cc
@@ -1,4 +1,3 @@
-#include <string>
 #include <iostream>
 using namespace std;
 
@@ -12,27 +11,18 @@ int main()
 		if (i < input)
 		{
 			for (int j = 0; j < input - i - 1; j++)
-			{
 				cout << ' ';
-			}
 
 			for (int j = 0; j < i + 1; j++)
-			{
 				cout << '*';
-			}
 		}
 		else
 		{
 			for (int j = 0; j < i - input + 1; j++)
-			{
 				cout << ' ';
-			}
 
 			for (int j = i; j < input * 2 - 1; j++)
-			{
 				cout << '*';
-				
-			}
 		}
 
 		cout << endl;
