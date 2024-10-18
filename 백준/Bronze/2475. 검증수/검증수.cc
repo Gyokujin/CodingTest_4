fe53e42df35ddacf57eaf26980d69adcdb7af3a9
Gyokujin <dd43454548@naver.com>
2024-10-18 14:54:52 +0900

    [Bronze V] Title: 검증수, Time: 0 ms, Memory: 2052 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int sum = 0;
+	int input;
+
+	for (int i = 0; i < 5; i++)
+	{
+		cin >> input;
+		sum += input * input;
+	}
+
+	cout << sum % 10;
+}
