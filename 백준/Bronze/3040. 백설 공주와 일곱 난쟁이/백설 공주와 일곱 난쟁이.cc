69d0f111338e31e1906b91e5fdd3a8dffcb1469f
Gyokujin <dd43454548@naver.com>
2024-10-25 17:13:38 +0900

    [Bronze II] Title: 백설 공주와 일곱 난쟁이, Time: 0 ms, Memory: 2056 KB -BaekjoonHub

---
+++ 백준/Bronze/3040. 백설 공주와 일곱 난쟁이/백설 공주와 일곱 난쟁이.cc
@@ -0,0 +1,40 @@
+#include <vector>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	vector<int> dwarfs;
+	int overHeight = -100;
+
+	for (int i = 0; i < 9; i++)
+	{
+		int height;
+		cin >> height;
+		dwarfs.push_back(height);
+		overHeight += height;
+	}
+
+	for (int i = 0; i < dwarfs.size() - 1; i++)
+	{
+		bool isFind = false;
+
+		for (int j = i + 1; j < dwarfs.size(); j++)
+		{
+			if (dwarfs[i] + dwarfs[j] == overHeight)
+			{
+				isFind = true;
+				dwarfs.erase(dwarfs.begin() + j); // i보다 더 뒤에 있는 j부터 먼저 지운다.
+				dwarfs.erase(dwarfs.begin() + i);
+				break;
+			}
+		}
+
+		if (isFind)
+			break;
+	}
+
+	for (int num : dwarfs)
+		cout << num << endl;
+}
