9b38a339bf4a8512344375d661b0668434adfe62
Gyokujin <dd43454548@naver.com>
2024-11-03 03:46:27 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2056 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.cc
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -2,22 +2,22 @@
 using namespace std;
 
 long long M = 1234567891; // 서로소
+int a = 1;
 int r = 31;
 
 int main()
 {
-	int R = 1;
+	long long answer = 0;
 	int L;
 	string word;
-	long long answer = 0;
 
 	cin >> L;
 	cin >> word;
 
-	for (int i = 0; i < L; i++)
+	for (char c : word)
 	{
-		answer += ((int)word[i] - 96) * R % M;
-		R = R * r % M;
+		answer = (answer + ((int)c - 96) * a) % M;
+		a = a * r % M;
 	}
 
 	cout << answer;
