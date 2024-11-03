58ed45a7e41843951bae9d2578e59db10c2a7850
Gyokujin <dd43454548@naver.com>
2024-11-04 01:54:35 +0900

    [Bronze I] Title: 소인수분해, Time: 28 ms, Memory: 2056 KB -BaekjoonHub

---
+++ 백준/Bronze/11653. 소인수분해/소인수분해.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int N;
+	cin >> N;
+
+	for (int i = 2; i <= N; i++)
+	{
+		if (N % i == 0)
+		{
+			cout << i << endl;
+			N /= i;
+			i = 1; // 사용한 약수는 초기화
+		}
+
+		if (N == 1)
+			break;
+	}
+}
