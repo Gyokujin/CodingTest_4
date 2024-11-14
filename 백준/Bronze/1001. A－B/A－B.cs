0a785e6d44a85142b0abbd46ccb3760f1752b101
Gyokujin <dd43454548@naver.com>
2024-11-15 03:40:59 +0900

    [Bronze V] Title: A-B, Time: 52 ms, Memory: 5512 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] inputs = Console.ReadLine().Split();
+        int A = int.Parse(inputs[0]);
+        int B = int.Parse(inputs[1]);
+        Console.Write(A - B);
+    }
+}
