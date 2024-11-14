9fe01bf50276d5c38eb7b919bccf4346b76cc6a4
Gyokujin <dd43454548@naver.com>
2024-11-15 06:10:28 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 208 ms, Memory: 6568 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class Example
+{
+    public static void Main()
+    {
+        int N = int.Parse(Console.ReadLine());
+        
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = 0; j <= i; j++)
+                Console.Write('*');
+            
+            Console.WriteLine();
+        }
+    }
+}
