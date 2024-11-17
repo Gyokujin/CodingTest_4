3dba896831889402979e3a142d8e8442c46bbb89
Gyokujin <dd43454548@naver.com>
2024-11-18 08:24:41 +0900

    [Bronze II] Title: 니모를 찾아서, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Bronze/10173. 니모를 찾아서/니모를 찾아서.cc
+++ 백준/Bronze/10173. 니모를 찾아서/니모를 찾아서.cc
@@ -1,37 +1,19 @@
 #include <string>
+#include <algorithm>
+#include <cctype>
 #include <iostream>
 using namespace std;
 
 int main()
 {
-    while (true)
+    string word;
+
+    while (getline(cin, word) && word != "EOI")
     {
-        string word;
-        getline(cin, word);
+        transform(word.begin(), word.end(), word.begin(),
+            [](unsigned char c) { return toupper(c); });
 
-        if (word == "EOI")
-            break;
-
-        bool able = false;
-
-        if (word.size() >= 4)
-        {
-            for (int i = 0; i < word.size() - 3; i++)
-            {
-                string curWord = "";
-
-                for (int j = i; j < i + 4; j++)
-                    curWord += toupper(word[j]);
-
-                if (curWord == "NEMO")
-                {
-                    able = true;
-                    break;
-                }
-            }
-        }
-
-        if (able)
+        if (word.find("NEMO") != string::npos)
             cout << "Found" << endl;
         else
             cout << "Missing" << endl;
