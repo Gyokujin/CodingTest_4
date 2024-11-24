0290fca5d28b342f7f0363aef6fed8e4c3dae476
Gyokujin <dd43454548@naver.com>
2024-11-24 11:31:39 +0900

    [Bronze IV] Title: 파티가 끝나고 난 뒤, Time: 0 ms, Memory: 2056 KB -BaekjoonHub

---
+++ 백준/Bronze/2845. 파티가 끝나고 난 뒤/파티가 끝나고 난 뒤.cc
@@ -0,0 +1,17 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int L, P;
+	vector<int> numbers(5);
+
+	cin >> L >> P;
+
+	for (int i = 0; i < numbers.size(); i++)
+		cin >> numbers[i];
+
+	for (int num : numbers)
+		cout << num - L * P << ' ';
+}
