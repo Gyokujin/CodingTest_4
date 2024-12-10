afc5f66eeea1ad81518873729eb03a1f3f41fe0a
Gyokujin <dd43454548@naver.com>
2024-12-10 10:09:23 +0900

    [Bronze III] Title: 더하기 3, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

---
+++ 백준/Bronze/11023. 더하기 3/더하기 3.cc
@@ -0,0 +1,26 @@
+#include <string>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	string S;
+	getline(cin, S);
+
+	string curWord = "";
+	int sum = 0;
+
+	for (char c : S)
+	{
+		if (c == ' ')
+		{
+			sum += stoi(curWord);
+			curWord = "";
+		}
+		else
+			curWord += c;
+	}
+
+	sum += stoi(curWord);
+	cout << sum;
+}
