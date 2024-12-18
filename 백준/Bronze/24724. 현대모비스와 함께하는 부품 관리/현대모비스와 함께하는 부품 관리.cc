8310659a416fd8f2de995a4076d3225c71ea3d7c
Gyokujin <dd43454548@naver.com>
2024-12-19 03:41:24 +0900

    [Bronze III] Title: 현대모비스와 함께하는 부품 관리, Time: 104 ms, Memory: 2056 KB -BaekjoonHub

---
+++ 백준/Bronze/24724. 현대모비스와 함께하는 부품 관리/현대모비스와 함께하는 부품 관리.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int T;
+	cin >> T;
+
+	for (int i = 0; i < T; i++)
+	{
+		int N, A, B;
+		cin >> N;
+		cin >> A >> B;
+
+		for (int j = 0; j < N; j++)
+		{
+			int size, weight;
+			cin >> size >> weight;
+		}
+
+		cout << "Material Management " << i + 1 << endl;
+		cout << "Classification ---- End!" << endl;
+	}
+}
